2563509ba4eca5a418ef1ea428d45be8bc076d4c
hdpyo <vvhooping@gmail.com>
2023-04-28 19:23:20 +0900

    feat: 소개 페이지 및 Footer 생성

---
+++ src/components/Common/Footer.tsx
@@ -0,0 +1,24 @@
+import React, { FunctionComponent } from 'react'
+
+import styled from '@emotion/styled'
+
+const FooterWrapper = styled.div`
+  display: grid;
+  place-items: center;
+  margin-top: auto;
+  padding: 50px 0;
+  font-size: 15px;
+  text-align: center;
+  line-height: 1.5;
+`
+
+const Footer: FunctionComponent = function () {
+  return (
+    <FooterWrapper>
+      Thank You for Visiting My Blog, Have a Good Day 😄
+      <br />© 2023 Developer Whooping, Powered By Gatsby.
+    </FooterWrapper>
+  )
+}
+
+export { Footer }
