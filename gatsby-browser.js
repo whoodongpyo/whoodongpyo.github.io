1c5a4062034230b7bf0841612199f12679c30218
hdpyo <vvhooping@gmail.com>
2023-04-29 23:15:40 +0900

    chore: 설치한 플러그인 관련 설정

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,3 +5,7 @@
  */
 
 // You can delete this file if you're not using it
+
+// Markdown 내 코드 테마 적용하기 (테마 불러오기)
+// import 'prismjs/themes/prism-tomorrow.css'
+import 'prismjs/themes/prism-solarizedlight.css'
