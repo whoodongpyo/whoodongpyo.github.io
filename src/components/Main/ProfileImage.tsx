bd138798a87d4edac267b1f765006df0e52b2ee1
hdpyo <vvhooping@gmail.com>
2023-04-30 11:38:25 +0900

    refactor: 프로필 이미지 변경

--- src/components/Main/ProfileImage.tsx
+++ src/components/Main/ProfileImage.tsx
@@ -4,7 +4,7 @@ import styled from '@emotion/styled'
 
 // 자신이 원하는 프로필 이미지 링크로 설정하기
 const PROFILE_IMAGE_LINK =
-  'https://avatars.githubusercontent.com/u/24629040?s=460&u=0bb3411f25c0e1c5d25d753fc648739367cb7032&v=4'
+  'https://avatars.githubusercontent.com/u/55405713?v=4'
 
 const ProfileImageWrapper = styled.img`
   width: 120px;
