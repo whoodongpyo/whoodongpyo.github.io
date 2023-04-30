f48b97675deb845b29135496648b282dd379b951
VVhooping <vvhooping@gmail.com>
2023-05-01 00:02:14 +0900

    style: CommentWidget 컴포넌트에 반응형 디자인 추가
    - Utterances 위젯은 기본적으로 반응형이기 때문에 좌우 padding만 추가로
      설정함.

--- src/components/Post/CommentWidget.tsx
+++ src/components/Post/CommentWidget.tsx
@@ -1,4 +1,5 @@
 import React, { createRef, FunctionComponent, useEffect } from 'react'
+import styled from '@emotion/styled'
 
 const src = 'https://utteranc.es/client.js'
 const repo = 'whoodongpyo/whoodongpyo.github.io'
@@ -12,6 +13,12 @@ type UtterancesAttributesType = {
   crossorigin: string
   async: string
 }
+
+const UtterancesWrapper = styled.div`
+  @media (max-width: 768px) {
+    padding: 0 20px;
+  }
+`
 
 const CommentWidget: FunctionComponent = function () {
   const element = createRef<HTMLDivElement>()
@@ -39,7 +46,7 @@ const CommentWidget: FunctionComponent = function () {
     element.current.appendChild(utterances)
   }, [])
 
-  return <div ref={element}></div>
+  return <UtterancesWrapper ref={element}></UtterancesWrapper>
 }
 
 export default CommentWidget
