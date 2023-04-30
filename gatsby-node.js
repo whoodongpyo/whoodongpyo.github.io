bdde0b7d12ce421409496ce18c78e0e5aa1eaa25
VVhooping <vvhooping@gmail.com>
2023-04-30 18:26:06 +0900

    feat: 마크다운 데이터에 Slug 필드를 추가하라
    - Gatsby에서 제공하는 onCreateNode API 를 활용한다.

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,23 +1,5 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-node/
- */
-
-/**
- * @type {import('gatsby').GatsbyNode['createPages']}
- */
-// exports.createPages = async ({ actions }) => {
-//   const { createPage } = actions
-//   createPage({
-//     path: "/using-dsg",
-//     component: require.resolve("./src/templates/using-dsg.js"),
-//     context: {},
-//     defer: true,
-//   })
-// }
-
 const path = require('path')
+const { createFilePath } = require(`gatsby-source-filesystem`)
 
 // Setup Import Alias
 exports.onCreateWebpackConfig = ({ getConfig, actions }) => {
@@ -33,4 +15,15 @@ exports.onCreateWebpackConfig = ({ getConfig, actions }) => {
       },
     },
   })
+}
+
+// Generate a Slug Each Post Data
+exports.onCreateNode = ({ node, getNode, actions }) => {
+  const { createNodeField } = actions
+
+  if (node.internal.type === 'MarkdownRemark') {
+    const slug = createFilePath({ node, getNode })
+
+    createNodeField({ node, name: 'slug', value: slug })
+  }
 }
