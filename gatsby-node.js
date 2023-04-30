25b73e8180ffb1fd060f0d742bf5410b6a42a831
VVhooping <vvhooping@gmail.com>
2023-04-30 21:18:15 +0900

    feat: 템플릿 컴포넌트로 게시글 페이지 생성하기

--- gatsby-node.js
+++ gatsby-node.js
@@ -27,3 +27,60 @@ exports.onCreateNode = ({ node, getNode, actions }) => {
     createNodeField({ node, name: 'slug', value: slug })
   }
 }
+
+// Generate Post Page Throught Markdown Data
+exports.createPages = async ({ actions, graphql, reporter }) => {
+  const { createPage } = actions
+
+  // Get All Markdown File For Paging
+  const queryAllMarkdownData = await graphql(
+    `
+      {
+        allMarkdownRemark(
+          sort: {
+            order: DESC
+            fields: [frontmatter___date, frontmatter___title]
+          }
+        ) {
+          edges {
+            node {
+              fields {
+                slug
+              }
+            }
+          }
+        }
+      }
+    `,
+  )
+
+  // Handling GraphQL Query Error
+  if (queryAllMarkdownData.errors) {
+    reporter.panicOnBuild(`Error While running query`)
+    return
+  }
+
+  // Import Post Template Component
+  const PostTemplateComponent = path.resolve(
+    __dirname,
+    'src/templates/post_template.tsx',
+  )
+
+  // Page Generating Function : slug 데이터를 통해 페이지를 생성하는 함수
+  const generatePostPage = ({
+    node: {
+      fields: { slug },
+    },
+  }) => {
+    const pageOptions = {
+      path: slug,
+      component: PostTemplateComponent,
+      context: { slug },
+    }
+
+    createPage(pageOptions) // createPage: Gatsby API
+  }
+
+  // Generate Post Page And Passing Slug Props for Query
+  queryAllMarkdownData.data.allMarkdownRemark.edges.forEach(generatePostPage)
+}
